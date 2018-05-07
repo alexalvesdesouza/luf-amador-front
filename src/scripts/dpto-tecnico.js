8ef24f0c01731d1c0f19fed524217414535cc5ef
Alex Alves de Souza <alexalvesdesouza@outlook.com>
2018-05-07 20:11:44 -0300

    Add imagens

--- src/scripts/dpto-tecnico.js
+++ src/scripts/dpto-tecnico.js
@@ -1,7 +1,28 @@
 var dptoTecnico = angular.module("dptoTecnico", []);
 dptoTecnico.controller("dptoTecnicoCtrl", function ($scope, $filter, $http) {
 
-    $scope.titulo = 'Classificação';
-    $scope.headerContent = 'Classificação';
+    $scope.titulo = 'Tabela Jogos';
+    $scope.headerContent = 'Tabela Jogos';
+
+    $scope.tabelaJogos = [];
+    $scope.tabelaCampeonato = {};
+    const BASE_PATH = "http://localhost:9090/tabela-jogos";
+
+    var carregarTabelasJogos = function () {
+        $http
+            .get(BASE_PATH)
+            .success(function (ret) {
+                $scope.tabelaJogos = [];
+                $scope.tabelaJogos = ret[0].jogos;
+            })
+            .error(function (data, status) {
+                // Handle HTTP error
+                console.log(data);
+            });
+
+
+    };
+
+    carregarTabelasJogos();
 
 });
